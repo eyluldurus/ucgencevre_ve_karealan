14d5d3b968fd130cd0c689328e6a696a3531d44b
eylül <eyll.durus@gmail.com>
2024-10-12 12:35:23 +0300

    toint32 fonksiyonundan sonraki boşluğu sildim

--- Program.cs
+++ Program.cs
@@ -4,13 +4,13 @@ using System.Reflection.Metadata.Ecma335;
 int kenar1; int kenar2; int kenar3; int cevre; double kenar; double alan; 
 
 Console.WriteLine("birinci kenar değeri gir");
-kenar1 = Convert.ToInt32 (Console.ReadLine());
+kenar1 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("ikinci kenar değeri gir");
-kenar2 = Convert.ToInt32 (Console.ReadLine());
+kenar2 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("üçüncü kenar değeri gir");
-kenar3 = Convert.ToInt32 (Console.ReadLine());
+kenar3 = Convert.ToInt32(Console.ReadLine());
 
 cevre = (kenar1 + kenar2 + kenar3);
 Console.WriteLine("Üçgenin çevresi:" + cevre);
